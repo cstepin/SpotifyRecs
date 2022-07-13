1182df5868b9d75996f2c4166542f8b559a04061
Christina Stepin <cstepin@fb.com>
2022-07-13 11:05:24 -0700

    First attempt at using spotify's algoirthm in new adapter

--- app/src/main/java/com/example/spotifyrecs/ExportActivity.java
+++ app/src/main/java/com/example/spotifyrecs/ExportActivity.java
@@ -1,31 +1,46 @@
 package com.example.spotifyrecs;
 
+import static com.example.spotifyrecs.resources.Resources.getClientId;
+import static com.example.spotifyrecs.resources.Resources.getRedirectUrl;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ProgressBar;
 
 import com.example.spotifyrecs.adapters.SwipeSongAdapter;
 import com.example.spotifyrecs.adapters.SwipeSongDeckAdapter;
 import com.example.spotifyrecs.models.Song;
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.ContentApi;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+import com.spotify.protocol.client.CallResult;
+import com.spotify.protocol.types.ListItem;
+import com.spotify.protocol.types.ListItems;
 import com.yalantis.library.Koloda;
 import com.yalantis.library.KolodaListener;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 
 public class ExportActivity extends AppCompatActivity {
 
+
+    private SpotifyAppRemote mSpotifyAppRemote;
     List<Song> songs = new ArrayList<>();
-
     Koloda koloda;
-
     protected SwipeSongDeckAdapter adapter;
+    ProgressBar pb;
+    final String TAG = "ExportActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +48,16 @@ public class ExportActivity extends AppCompatActivity {
         setContentView(R.layout.activity_export);
 
         Koloda koloda = findViewById(R.id.koloda);
+        pb = findViewById(R.id.pbLoading);
+    }
 
-        songs.add(new Song("ex", "1"));
-        songs.add(new Song("ex2", "2"));
-        songs.add(new Song("ex3", "3"));
+    private void querySongs(List<Song> finalSongs) {
+
+        Log.i(TAG, "length: " + finalSongs.size());
+        songs.addAll(finalSongs);
+       // adapter.notifyDataSetChanged();
 
         adapter = new SwipeSongDeckAdapter(this, songs);
-
         koloda.setAdapter(adapter);
 
         koloda.setKolodaListener(new KolodaListener() {
@@ -93,5 +111,115 @@ public class ExportActivity extends AppCompatActivity {
 
             }
         });
+
+// run a background job and once complete
+        pb.setVisibility(ProgressBar.INVISIBLE);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i("in onStart", "in Onstart");
+
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(getClientId())
+                        .setRedirectUri(getRedirectUrl())
+                        .showAuthView(true)
+                        .build();
+
+        SpotifyAppRemote.connect(this, connectionParams,
+                new Connector.ConnectionListener() {
+
+                    @Override
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.d(TAG, "Connected! Yay!");
+
+                        // Now you can start interacting with App Remote
+                        connected();
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.e(TAG, throwable.getMessage(), throwable);
+
+                    }
+                });
+    }
+
+    private void connected() {
+        // Play a playlist
+
+        List<Song> songs = new ArrayList<>();
+
+        // This gets the content asynchronously in recommended
+
+        CallResult<ListItems> listItemsCallResult = mSpotifyAppRemote.getContentApi()
+                .getRecommendedContentItems(ContentApi.ContentType.DEFAULT);
+        listItemsCallResult.setResultCallback(new CallResult.ResultCallback<ListItems>() {
+            @Override
+            public void onResult(ListItems data) {
+                if(data != null){
+                    Log.i("first", "first on Result");
+                    ListItem[] items = data.items;
+
+                    for(int i = 0; i < items.length; i++) {
+                        //     for (ListItem item : items) {
+                        CallResult<ListItems> listItemsCallResult1 = mSpotifyAppRemote
+                                .getContentApi()
+                                .getChildrenOfItem(items[i], 3, 0);
+
+                        int finalI = i;
+                        listItemsCallResult1.setResultCallback(new CallResult.ResultCallback<ListItems>() {
+                            @Override
+                            public void onResult(ListItems data) {
+                                if (data != null) {
+                                    ListItem[] items1 = data.items;
+
+                                    for (ListItem item1 : items1) {
+
+                                        if (item1.uri.contains("playlist") ||
+                                                item1.uri.contains("track") ||
+                                                item1.uri.contains("album")){
+                                            if(!InList(item1.title, songs)) {
+                                                Song currSong = new Song();
+                                                currSong.title = item1.title;
+                                                currSong.uri = item1.uri;
+                                                currSong.artist = item1.subtitle;
+                                                currSong.imageString = item1.imageUri.raw;
+
+                                                songs.add(currSong);
+                                                Log.i("fsd", "inside + " + item1 + " and title: " + item1);
+                                            }
+                                        }
+                                    }
+                                    if(finalI == items.length - 1){
+                                        Collections.shuffle(songs);
+                                        querySongs(songs);
+                                    }
+                                }
+                            }
+                        });
+                    }
+                }
+            }
+        });
+    }
+
+    protected Boolean InList(String title, List<Song> songs){
+        for(Song song : songs){
+            if(song.title.equals(title)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Aaand we will finish off here.
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
     }
 }
--- app/src/main/java/com/example/spotifyrecs/adapters/SwipeSongDeckAdapter.java
+++ app/src/main/java/com/example/spotifyrecs/adapters/SwipeSongDeckAdapter.java
@@ -1,7 +1,13 @@
 package com.example.spotifyrecs.adapters;
 
+import static com.example.spotifyrecs.resources.Resources.getClientId;
+import static com.example.spotifyrecs.resources.Resources.getRedirectUrl;
+
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -9,17 +15,32 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.palette.graphics.Palette;
+
 import com.example.spotifyrecs.R;
 import com.example.spotifyrecs.models.Song;
+import com.parse.ParseUser;
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
+import com.spotify.protocol.client.CallResult;
+import com.spotify.protocol.types.ImageUri;
 
-import java.util.ArrayList;
+import org.json.JSONArray;
+
 import java.util.List;
 
 public class SwipeSongDeckAdapter extends BaseAdapter {
 
+    TextView tvTitle;
+    TextView tvArtist;
+    ImageView ivCoverArt;
+    ImageButton ibPlay;
+    Boolean pressed = false;
+
     private Context context;
     private List<Song> songs;
+    private SpotifyAppRemote mSpotifyAppRemote;
 
     public SwipeSongDeckAdapter(Context context, List<Song> songs) {
         this.context = context;
@@ -32,7 +53,7 @@ public class SwipeSongDeckAdapter extends BaseAdapter {
     }
 
     @Override
-    public Object getItem(int position) {
+    public Song getItem(int position) {
         return songs.get(position);
     }
 
@@ -43,8 +64,6 @@ public class SwipeSongDeckAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-     //   TextView tvTitle;
-     //   TextView tvArtist;
 
         // in get view method we are inflating our layout on below line.
         View v = convertView;
@@ -55,9 +74,126 @@ public class SwipeSongDeckAdapter extends BaseAdapter {
         }
 
         // on below line we are initializing our variables and setting data to our variables.
-        ((TextView) v.findViewById(R.id.tvTitle)).setText(songs.get(position).getTitle());
-        ((TextView) v.findViewById(R.id.tvArtist)).setText(songs.get(position).getArtist());
+        tvArtist = (TextView) v.findViewById(R.id.tvArtist);
+        tvTitle = (TextView) v.findViewById(R.id.tvTitle);
+        ivCoverArt = (ImageView) v.findViewById(R.id.ivCoverArt);
+        ibPlay = (ImageButton) v.findViewById(R.id.ibPlay);
+
+        ibPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPlay((Song) getItem(position), true, v);
+            }
+        });
+
+        startPlay((Song) getItem(position), false, v);
+        tvArtist.setText(songs.get(position).getArtist());
+        tvTitle.setText(songs.get(position).getTitle());
+
+        // (ImageView) v.findViewById(R.id.ivCoverArt)...;
 
         return v;
     }
+
+    public void startPlay(Song s, Boolean onClick, View v) {
+
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(getClientId())
+                        .setRedirectUri(getRedirectUrl())
+                        .showAuthView(true)
+                        .build();
+
+        SpotifyAppRemote.connect(context, connectionParams,
+                new Connector.ConnectionListener() {
+                    //Connects with the Android SDK
+                    @Override
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.d("Adapter", "Connected! Yay!");
+                        // Now you can start interacting with App Remote
+                        connected(s, onClick, v);
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.e("Adapter", throwable.getMessage(), throwable);
+                    }
+                });
+    }
+
+    private void connected(Song s, Boolean onClick, View v) {
+        if (onClick) {
+            if (!pressed) {
+                // Play a song
+                mSpotifyAppRemote.getPlayerApi().play(s.uri);
+                pressed = true;
+            } else {
+                mSpotifyAppRemote.getPlayerApi().pause();
+                pressed = false;
+            }
+        } else {
+            //Sets up background by converting image to bitmap then finding the brightest color
+            // and setting the background to be that color
+            Log.i("adapter", "in image string: " + s.imageString);
+            ImageUri newUri = new ImageUri(s.imageString);
+            CallResult<Bitmap> bitmapCallResult = mSpotifyAppRemote.getImagesApi()
+                    .getImage(newUri);
+            bitmapCallResult.setResultCallback(data -> {
+                Log.i("adapter", "in on result bit map");
+                Palette palette = Palette.from(data).generate();
+                Palette.Swatch vibrant = palette.getVibrantSwatch();
+                if (vibrant != null) {
+                    // Set the background color of a layout based on the vibrant color
+                    v.setBackgroundColor(vibrant.getRgb());
+                }
+                ivCoverArt.setImageBitmap(data);
+            });
+        }
+    }
+
+    /*
+    private void onSongClick(View v, MotionEvent event, long lastClickTime){
+        final long DOUBLE_CLICK_TIME_DELTA = 300;//milliseconds
+        long clickTime = System.currentTimeMillis();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if (clickTime - lastClickTime < DOUBLE_CLICK_TIME_DELTA){
+                    Log.i("here2", "in double tap with delta time: " + (clickTime
+                            - lastClickTime));
+                    onDoubleClick(v);
+                }
+                break;
+        }
+    }
+
+    private void onDoubleClick(View v) {
+        Log.i("In double click2", "double click noticed");
+        Song song = new Song();
+        song.artist = (String) tvArtist.getText();
+        song.title = (String) tvTitle.getText();
+        //  Log.i("in double click3", "this is song: " + song.toString());
+        faveSongs.add(song.title);
+    }
+
+    private void updateLikedSongs() {
+        if(faveSongs.size() == 0){
+            return;
+        }
+
+        JSONArray currLiked = ParseUser.getCurrentUser().getJSONArray("faveSongs");
+        assert currLiked != null;
+        for(String song : faveSongs){
+            currLiked.put(song);
+        }
+        ParseUser.getCurrentUser().put("faveSongs", currLiked);
+        ParseUser.getCurrentUser().saveInBackground(e -> {
+            if(e != null){
+                Log.e("AddPlaylistFragment", "error saving playlists", e);
+            }
+            else{
+                Log.i("Addplaylistfragment", "faveSongs saved successfully");
+            }
+        });
+    }
+     */
 }
