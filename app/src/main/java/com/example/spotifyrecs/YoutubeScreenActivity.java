d90e028c95708e6602ca6c95b55ac387d3353220
Christina Stepin <cstepin@fb.com>
2022-07-27 16:52:50 -0700

    Having the youtube data api work about half of the time.

--- app/src/main/java/com/example/spotifyrecs/YoutubeScreenActivity.java
+++ app/src/main/java/com/example/spotifyrecs/YoutubeScreenActivity.java
@@ -1,7 +1,9 @@
 package com.example.spotifyrecs;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -24,6 +26,146 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import okhttp3.OkHttpClient;
+
+/**
+ * Sample Java code for youtube.search.list
+ * See instructions for running these code samples locally:
+ * https://developers.google.com/explorer-help/code-samples#java
+ */
+
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.SearchListResponse;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.Collection;
+
+/*
+public class YoutubeScreenActivity extends Activity {
+    // You need to set this value for your code to compile.
+    // For example: ... DEVELOPER_KEY = "YOUR ACTUAL KEY";
+    private static final String DEVELOPER_KEY = BuildConfig.YOUTUBE_API_KEY;
+
+    private static final String APPLICATION_NAME = "SpotifyRecs";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+
+  //  YouTubePlayerView youTubePlayerView;
+    YouTubePlayer.OnInitializedListener mOnInitializedListener;
+    ArrayList<String> mListData = new ArrayList<>();
+
+    final static String TAG = "YoutubeScreenActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_youtube_screen);
+
+  //      youTubePlayerView = findViewById(R.id.player);
+
+        Log.i(TAG, "here");
+        new TestAsync().execute();
+        //   mainFunction();
+    }
+
+    /**
+     * Build and return an authorized API client service.
+     *
+     * @return an authorized API client service
+     * @throws GeneralSecurityException, IOException
+     */
+
+/*
+    public static YouTube getService() throws GeneralSecurityException, IOException {
+     //   final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        final NetHttpTransport httpTransport = new com.google.api.client.http.javanet.NetHttpTransport();
+        Log.i(TAG, "success in service");
+        return new YouTube.Builder(httpTransport, JSON_FACTORY, null)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+
+    /**
+     * Call function to create API service object. Define and
+     * execute API request. Print API response.
+     *
+     * @throws GeneralSecurityException, IOException, GoogleJsonResponseException
+     */
+
+/*
+    public static void mainFunction()
+            throws GeneralSecurityException, IOException, GoogleJsonResponseException {
+        YouTube youtubeService = getService();
+        // Define and execute the API request
+        YouTube.Search.List request = youtubeService.search()
+                .list("");
+        SearchListResponse response = request.setKey(DEVELOPER_KEY)
+                .setQ("chopin")
+                .execute();
+        System.out.println(response);
+        Log.i("example", "this is the response: " + response);
+    }
+
+    static class TestAsync extends AsyncTask<Void, Integer, String> {
+        String TAG = getClass().getSimpleName();
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Log.d(TAG + " PreExceute","On pre Exceute......");
+        }
+
+        protected String doInBackground(Void...arg0) {
+            Log.d(TAG + " DoINBackGround", "On doInBackground...");
+
+            YouTube youtubeService = null;
+            try {
+                youtubeService = getService();
+            } catch (GeneralSecurityException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // Define and execute the API request
+            YouTube.Search.List request = null;
+            try {
+                request = youtubeService.search()
+                        .list("");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            SearchListResponse response = null;
+            try {
+                response = request.setKey(DEVELOPER_KEY)
+                        .setQ("chopin")
+                        .execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(response);
+
+            return "You are at PostExecute";
+        }
+
+        protected void onProgressUpdate(Integer...a) {
+            super.onProgressUpdate(a);
+       //     Log.d(TAG + " onProgressUpdate", "You are in progress update ... " + a[0]);
+        }
+
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Log.d(TAG + " onPostExecute", "" + result);
+        }
+    }
+}
+
+*/
 
 public class YoutubeScreenActivity extends YouTubeBaseActivity {
 
@@ -48,9 +190,11 @@ public class YoutubeScreenActivity extends YouTubeBaseActivity {
         OkHttpClient client = new OkHttpClient();
 
         if(getIntent().hasExtra("title")){
-            searchQuery += ((String)getIntent().getExtras().get("title")).replaceAll("\\s", "");;
-            searchQuery += ((String) getIntent().getExtras().get("artist")).replaceAll("\\s", "");;
-            CHANNEL_GET_URL = "https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&q="+searchQuery+"&maxResults=20&key="+BuildConfig.YOUTUBE_API_KEY+"";
+            searchQuery = "";
+            searchQuery += ((String)getIntent().getExtras().get("title")).replaceAll("\\s", "");
+            searchQuery += ((String) getIntent().getExtras().get("artist")).replaceAll("\\s", "");
+         //   searchQuery += "chopin_tristesse";
+            CHANNEL_GET_URL = "https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&q="+searchQuery+"&maxResults=20&order=viewCount&type=video&key="+BuildConfig.YOUTUBE_API_KEY+"";
 
             new RequestYoutubeAPI().execute();
         }
@@ -61,8 +205,8 @@ public class YoutubeScreenActivity extends YouTubeBaseActivity {
      * See instructions for running these code samples locally:
      * https://developers.google.com/explorer-help/code-samples#java
      */
-/*
 
+    /*
     public class ApiExample {
         private static final String CLIENT_SECRETS= "client_secret.json";
         private static final Collection<String> SCOPES =
@@ -77,7 +221,9 @@ public class YoutubeScreenActivity extends YouTubeBaseActivity {
          * @return an authorized Credential object.
          * @throws IOException
          */
+
     /*
+
         public static Credential authorize(final NetHttpTransport httpTransport) throws IOException {
             // Load client secrets.
             InputStream in = ApiExample.class.getResourceAsStream(CLIENT_SECRETS);
@@ -99,7 +245,8 @@ public class YoutubeScreenActivity extends YouTubeBaseActivity {
          * @throws GeneralSecurityException, IOException
          */
 
-    /*
+        /*
+
         public static YouTube getService() throws GeneralSecurityException, IOException {
             final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
             Credential credential = authorize(httpTransport);
@@ -140,6 +287,7 @@ public class YoutubeScreenActivity extends YouTubeBaseActivity {
             try {
                 HttpResponse response = httpClient.execute(httpGet);
                 HttpEntity httpEntity = response.getEntity();
+           //     Log.e("response", EntityUtils.toString(httpEntity));
                 return EntityUtils.toString(httpEntity);
 
             } catch (IOException e) {
